dbbcf819f167d307602cb71f7d91098238a898db
John Park <johnparkbluejay@gmail.com>
2024-09-17 17:58:05 -0400

    boat image and text box added

--- components/Guide.tsx
+++ components/Guide.tsx
@@ -1,7 +1,60 @@
-import React from "react";
+import Image from "next/image";
 
 const Guide = () => {
-	return <div></div>;
+	return (
+		<section className="flexCenter flex-col">
+			<div className="border-2 border-red-500 padding-container max-container w-full pb-24">
+				<Image src="/camp.svg" alt="camp" width={50} height={50} />
+				<p className="uppercase regular-18 mt-1 mb-3 text-green-50">
+					We are here for you
+				</p>
+				<div className="flex flex-wrap justify-between gap-5 lg:gap-10">
+					<h2 className="bold-40 lg:bold-64 xl:max-w-[390px]">
+						Guide you to Easy Path
+					</h2>
+					<p className="regular-16 text-gray-30 xl:max-w-[520px]">
+						Picture yourself setting up camp under a starlit sky, surrounded by
+						the sounds of wild Pokémon rustling in the grass and rivers flowing
+						nearby. As the campfire flickers, your Pokémon rest beside you,
+						ready for the next day's adventure. You'll explore lush forests,
+						climb rugged mountains, and encounter creatures in their natural
+						habitats. Camping in the Pokémon world offers a chance to connect
+						with nature and your team, making each night a peaceful retreat
+						before another day of discovery.
+					</p>
+				</div>
+			</div>
+
+			<div className="flexCenter max-container relative w-full border-2 border-blue-500">
+				<Image
+					src="/boat.png"
+					alt="boat"
+					width={1440}
+					height={580}
+					className="w-full object-cover object-center 2xl:rounded-5xl"
+				/>
+
+				<div className="absolute flex bg-white py-8 pl-5 pr-7 gap-3 rounded-3xl border shadow-md md:left-[5%] lg:top-20">
+					<div className="flexBetween flex-col">
+						<div className="flex w-full flex-col">
+							<div className="flexBetween w-full">
+								<p className="regular-16 text-gray-20">Destination</p>
+								<p className="bold-16 text-green-50">48 mins</p>
+							</div>
+							<p className="bold-20 mt-2">Aguas Calientes</p>
+						</div>
+
+						<div className="flex w-full flex-col">
+							<p className="regular-16 text-gray-20">Start track</p>
+							<h4 className="bold-20 mt-2 whitespace-nowrap">
+								Wonorejo Pasuruan
+							</h4>
+						</div>
+					</div>
+				</div>
+			</div>
+		</section>
+	);
 };
 
 export default Guide;
